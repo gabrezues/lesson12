8e22a9677667ea0c81d3ae71bdc968d7175d8e11
gabrezues <43757111+gabrezues@users.noreply.github.com>
2019-01-04 10:12:30 -0500

    Add files via upload

---
+++ ball.py
@@ -0,0 +1,29 @@
+import time
+import random
+
+print('-'*65)
+answer = input('What is your question? ')
+time.sleep(2)
+print('SHAKING')
+time.sleep(2)
+print('...thinking..')
+time.sleep(2)
+print('I gotchu fam')
+
+choice = randomint(1,6)
+
+if choice == 1:
+	print('Yes')
+elif choice == 2:
+	print('Nah dont')
+elif choice == 3:
+	print('Ask later idk lol')
+elif choice == 4:
+	print('Wouldnt dwell on it')
+elif choice == 5:
+	print('Not sure, you cant rely on that stupid Shinji')
+else choice:
+	print('DO WHAT YOU THINK')
+
+
+
